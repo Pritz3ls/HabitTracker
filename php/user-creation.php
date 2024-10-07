8f3d9a41ab3fbaa862b30e9948e790080d730fe8
devPritzels <princefinn14@gmail.com>
2024-10-07 18:35:03 +0800

    Refactor Captcha condition on Two PHP files

--- php/user-creation.php
+++ php/user-creation.php
@@ -1,60 +1,58 @@
 <?php
     session_start();
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if(isset($_POST['create'])){
         // Fetch CAPTCHA input from the user and the generated CAPTCHA value
         $enteredCaptcha = $_POST['captchaInput'];
         $generatedCaptcha = $_POST['hiddenCaptcha'];
-    
+        
         // Check if CAPTCHA matches
-        // CAPTCHA is correct, proceed with login logic
-        if ($enteredCaptcha === $generatedCaptcha) {
-            // Handles User Creation
-            if(isset($_POST['create'])){
-                // Fetch all required information
-                $username = $_POST['username'];
-                $phonenumber = $_POST['phonenumber'];
-                $password = $_POST['password'];
-                $user_type = $_POST['user_type'];
-                
-                // Validation of Phone Number
-                // If phone number is used, throw an error
-                $phone_validation_query = "SELECT COUNT(*) phone_valid FROM `users` WHERE phone_number = '{$phonenumber}'";
-                $user_valid = mysqli_query($conn, $phone_validation_query);
-                
-                $row = mysqli_fetch_assoc($user_valid);
-                if($row['phone_valid'] == 1){
-                    echo "Phone number is already used.";
-                    return;
-                }        
-
-                $query = "INSERT INTO users(user_name, phone_number, user_type, password)
-                VALUES('{$username}','{$phonenumber}', '{$user_type}','{$password}')";
-
-                $add_data = mysqli_query($conn, $query);
-
-                // Check if the sql execution is successful
-                if(!$add_data){
-                    echo "Something went wrong!" . mysqli_connect_error($conn);
-                    return;
-                }
-
-                // Save the current User ID
-                $fetch_id_query = "SELECT * FROM `users` WHERE phone_number = '{$phonenumber}'";
-                $fetch_id = mysqli_query($conn, $fetch_id_query);
-                $fetch = mysqli_fetch_assoc($fetch_id);
-                $_SESSION['currentUserID'] = $fetch['id'];
-                
-                if($user_type == 'client'){
-                    Header('Location: testHabit.php');  
-                    exit;
-                }else{ /* Admin */
-                    Header('Location: testDashboard-Admin.php');  
-                    exit;
-                }
-            }
-        } else {
-            // CAPTCHA is incorrect, show an error message
+        // CAPTCHA is incorrect, show an error message and return
+        if ($enteredCaptcha != $generatedCaptcha) {
             echo "Incorrect CAPTCHA!";
+            return;
+        }
+
+        // Handles User Sign up
+        // Fetch all required information
+        $username = $_POST['username'];
+        $phonenumber = $_POST['phonenumber'];
+        $password = $_POST['password'];
+        $user_type = $_POST['user_type'];
+        
+        // Validation of Phone Number
+        // If phone number is used, throw an error
+        $phone_validation_query = "SELECT COUNT(*) phone_valid FROM `users` WHERE phone_number = '{$phonenumber}'";
+        $user_valid = mysqli_query($conn, $phone_validation_query);
+        
+        $row = mysqli_fetch_assoc($user_valid);
+        if($row['phone_valid'] == 1){
+            echo "Phone number is already used.";
+            return;
+        }        
+
+        $query = "INSERT INTO users(user_name, phone_number, user_type, password)
+        VALUES('{$username}','{$phonenumber}', '{$user_type}','{$password}')";
+
+        $add_data = mysqli_query($conn, $query);
+
+        // Check if the sql execution is successful
+        if(!$add_data){
+            echo "Something went wrong!" . mysqli_connect_error($conn);
+            return;
+        }
+
+        // Save the current User ID
+        $fetch_id_query = "SELECT * FROM `users` WHERE phone_number = '{$phonenumber}'";
+        $fetch_id = mysqli_query($conn, $fetch_id_query);
+        $fetch = mysqli_fetch_assoc($fetch_id);
+        $_SESSION['currentUserID'] = $fetch['id'];
+        
+        if($user_type == 'client'){
+            Header('Location: testHabit.php');  
+            exit;
+        }else{ /* Admin */
+            Header('Location: testDashboard-Admin.php');  
+            exit;
         }
     }
 ?>
--- php/user-login.php
+++ php/user-login.php
@@ -1,55 +1,54 @@
 <?php
     session_start();
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    if(isset($_POST['login'])){
         // Fetch CAPTCHA input from the user and the generated CAPTCHA value
         $enteredCaptcha = $_POST['captchaInput'];
         $generatedCaptcha = $_POST['hiddenCaptcha'];
-    
+        
         // Check if CAPTCHA matches
-        // CAPTCHA is correct, proceed with login logic
-        if ($enteredCaptcha === $generatedCaptcha) {
-            // Handles User Creation
-            if(isset($_POST['login'])){
-                // Fetch all required information
-                $username = $_POST['username'];
-                $password = $_POST['password'];
-                
-                // Fetch if the user exists
-                // If the user doesn't exist, throw an error
-                $user_login_query = "SELECT * FROM `users` 
-                WHERE user_name = '{$username}' AND password = '{$password}'";
-                $user_found = mysqli_query($conn, $user_login_query);
-                
-                // Check if the sql execution is successful
-                if(!$user_found){
-                    echo "Something went wrong!" . mysqli_connect_error($conn);
-                    return;
-                }
-                
-                if(mysqli_num_rows($user_found) == 0){
-                    echo "<h3>Wrong Username or Password!</h3>";
-                    return;
-                }
-
-                // Fetch all the rows from executed query
-                $row = mysqli_fetch_assoc($user_found);
-                
-                // Save the current user ID
-                $_SESSION['currentUserID'] = $row['id'];
-
-                // Divert the user to their respective pages
-                // Two users are expected, Client and Admin    
-                if($row['user_type'] == 'client'){
-                    Header('Location: testHabit.php');  
-                    exit;
-                }else{ /* Admin */
-                    Header('Location: testDashboard-Admin.php');  
-                    exit;
-                }
-            }
-        } else {
-            // CAPTCHA is incorrect, show an error message
+        // CAPTCHA is incorrect, show an error message and return
+        if ($enteredCaptcha != $generatedCaptcha) {
             echo "Incorrect CAPTCHA!";
+            return;
+        }
+        
+        // Handles User Login
+        // Fetch all required information
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        
+        // Fetch if the user exists
+        // If the user doesn't exist, throw an error
+        $user_login_query = "SELECT * FROM `users` 
+        WHERE user_name = '{$username}' AND password = '{$password}'";
+        $user_found = mysqli_query($conn, $user_login_query);
+        
+        // Check if the sql execution is successful
+        if(!$user_found){
+            echo "Something went wrong!" . mysqli_connect_error($conn);
+            return;
+        }
+        
+        if(mysqli_num_rows($user_found) == 0){
+            echo "<h3>Wrong Username or Password!</h3>";
+            return;
+        }
+
+        // Fetch all the rows from executed query
+        $row = mysqli_fetch_assoc($user_found);
+        
+        // Save the current user ID
+        $_SESSION['currentUserID'] = $row['id'];
+
+        // Divert the user to their respective pages
+        // Two users are expected, Client and Admin    
+        if($row['user_type'] == 'client'){
+            Header('Location: testHabit.php');  
+            exit;
+        }else{ /* Admin */
+            Header('Location: testDashboard-Admin.php');  
+            exit;
         }
     }
 ?>
