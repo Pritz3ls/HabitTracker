93649a79045a5f0cd8e9d0235ec5be762bdd587c
devPritzels <princefinn14@gmail.com>
2024-10-09 23:38:58 +0800

    Added profile pictures for landing page

--- testLanding.php
+++ testLanding.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="resource/application-icon.png" type="image/png">
-    <link rel="stylesheet" href="css/landing.css"> 
+    <link rel="stylesheet" href="css/landing.css?v=<?php echo time(); ?>"> 
     <title>habere | Welcome</title>
 </head>
 <body>
@@ -29,19 +29,19 @@
         <h1>😎 MEET THE TEAM 😎</h1>
         <div class="creators">
             <div class="creator">
-                <img src="path/to/preetam.jpg" alt="gyles" class="creator-image">
+                <img src="resource/cont-gyles-profile.jpg" alt="gyles" class="creator-image">
                 <h2>Hey, this is gyles pogi love iris robles :D</h2>
                 <p>(Frontend Developer)</p>
                 
             </div>
             <div class="creator">
-                <img src="path/to/sankalp.jpg" alt="prince" class="creator-image">
+                <img src="resource/cont-prince-profile.jpg" alt="prince" class="creator-image">
                 <h2>Hi, this is prince marco:)</h2>
                 <p>(Project Lead and Backend Developer) </p>
                
             </div>
             <div class="creator">
-                <img src="resource/Ian pic.jpg" alt="ian" class="creator-image">
+                <img src="resource/cont-ian-profile.jpg" alt="ian" class="creator-image">
                 <h2>Hi, this is ian carlo:)</h2>
                 <p> (Backend Developer)</p>
                 
