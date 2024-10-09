addf67b0b9648418641b4b1837be3dd1e392d29d
Ian Zara <ianzara0718@gmail.com>
2024-10-09 01:11:53 -0700

    added habere icon

--- testDashboard-Admin.php
+++ testDashboard-Admin.php
@@ -6,6 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="habere-high-resolution-logo__3_-removebg-preview.png" type="image/png">
     <title>Admin Dashboard</title>
     <link rel="stylesheet" href="css/Admindashboard.css"> 
 </head>
--- testForgotPassword.php
+++ testForgotPassword.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="habere-high-resolution-logo__3_-removebg-preview.png" type="image/png">
     <link rel="stylesheet" href="css/forgotpass.css">
     <title>Test Forgot Password</title>
 </head>
