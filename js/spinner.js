36af8fd92a15642b1b98c4b46175588b840a9f03
devPritzels <princefinn14@gmail.com>
2024-10-29 21:15:45 +0800

    Font family changes

--- js/spinner.js
+++ js/spinner.js
@@ -3,4 +3,4 @@
 const loading = document.getElementById('loading');
 setTimeout(function() {
     loading.classList.add("hide-loader"); // Hide the loading animation
-}, 500); // Add a .5 second delay to simulate a slower page for the loading animation
+}, 100); // Add a .5 second delay to simulate a slower page for the loading animation
